416b63d394e90c390b85884298844041fda6b647
Fred Grott <fred.grott@gmail.com>
2012-01-26 13:44:20 -0600

    removed jdepend task add later, did a first test things work without the eclipse.running check..codeqa reports generated and converted to html and javadoc was generated.  Now need to do the project website boilerplate on the gh-pages branch

---
+++ project.docs/javadocs/reference/lists.js
@@ -0,0 +1,10 @@
+var DATA = [
+      { id:0, label:"com.github.shareme.androidcmwrench", link:"com/github/shareme/androidcmwrench/package-summary.html", type:"package" },
+      { id:1, label:"com.github.shareme.androidcmwrench.AndroidCMWrenchActivity", link:"com/github/shareme/androidcmwrench/AndroidCMWrenchActivity.html", type:"class" },
+      { id:2, label:"com.github.shareme.androidcmwrench.R", link:"com/github/shareme/androidcmwrench/R.html", type:"class" },
+      { id:3, label:"com.github.shareme.androidcmwrench.R.attr", link:"com/github/shareme/androidcmwrench/R.attr.html", type:"class" },
+      { id:4, label:"com.github.shareme.androidcmwrench.R.drawable", link:"com/github/shareme/androidcmwrench/R.drawable.html", type:"class" },
+      { id:5, label:"com.github.shareme.androidcmwrench.R.layout", link:"com/github/shareme/androidcmwrench/R.layout.html", type:"class" },
+      { id:6, label:"com.github.shareme.androidcmwrench.R.string", link:"com/github/shareme/androidcmwrench/R.string.html", type:"class" }
+
+    ];
